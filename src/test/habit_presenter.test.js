860672ad2a27e041b11fb8d1c530cb7221afb075
Chanwoo <tree698@gmail.com>
2022-11-14 19:59:06 +0900

    unit test - habit tracker

---
+++ src/test/habit_presenter.test.js
@@ -0,0 +1,82 @@
+import HabitPresenter from '../habit_presenter';
+
+describe('HabitPresenter', () => {
+  const habits = [
+    { id: 1, name: 'Reading', count: 1 },
+    { id: 2, name: 'Running', count: 0 },
+  ];
+  let presenter;
+  let update;
+
+  beforeEach(() => {
+    presenter = new HabitPresenter(habits, 3);
+    update = jest.fn();
+  });
+
+  it('inits with habits', () => {
+    expect(presenter.getHabits()).toEqual(habits);
+  });
+
+  it('increments habit count and call update callback', () => {
+    presenter.increment(habits[0], update);
+    expect(presenter.getHabits()[0].count).toBe(2);
+    checkUpdateIsCalled();
+  });
+
+  it('decrements habit count and call update callback', () => {
+    presenter.decrement(habits[0], update);
+    expect(presenter.getHabits()[0].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  it('does not set the count value below 0 when decrements', () => {
+    presenter.decrement(habits[0], update);
+    presenter.decrement(habits[0], update);
+    expect(presenter.getHabits()[0].count).toBe(0);
+    // checkUpdateIsCalled();
+  });
+
+  it('deletes habit and call update callback', () => {
+    presenter.delete(habits[0], update);
+    expect(presenter.getHabits().length).toBe(1);
+    expect(presenter.getHabits()[0].name).toBe('Running');
+    checkUpdateIsCalled();
+  });
+
+  it('adds habit and call update callback', () => {
+    presenter.add('Eating', update);
+    expect(presenter.getHabits().length).toBe(3);
+    expect(presenter.getHabits()[2].name).toBe('Eating');
+    expect(presenter.getHabits()[2].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  it('throws an error when the max habits limit is exceeded', () => {
+    presenter.add('sleeping', update);
+    expect(() => {
+      presenter.add('sleeping', update);
+    }).toThrow(`습관의 개수는 3개 이상이 될수 없습니다`);
+  });
+
+  describe('resets', () => {
+    it('resets all habits count to 0 and update callback', () => {
+      presenter.reset(update);
+      expect(presenter.getHabits()[0].count).toBe(0);
+      expect(presenter.getHabits()[1].count).toBe(0);
+      checkUpdateIsCalled();
+    });
+
+    it('does not create new object when count is 0', () => {
+      const habits = presenter.getHabits();
+      presenter.reset(update);
+      const updatedHabits = presenter.getHabits();
+      expect(updatedHabits[1]).toBe(habits[1]);
+      expect(updatedHabits[1]).toEqual(habits[1]); //always success
+    });
+  });
+
+  function checkUpdateIsCalled() {
+    expect(update).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledWith(presenter.getHabits());
+  }
+});
